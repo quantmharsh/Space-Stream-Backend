ca726f08148d66f93a02a82d5666aea5b883787f
Harsh Srivastava <srivastavah240@gmail.com>
2024-04-28 13:55:56 +0530

    deployment ready

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ import postRoutes from"./routes/postRoutes.js"
 import messageRoutes from"./routes/messageRoutes.js"
 import {v2 as cloudinary} from "cloudinary"
 import {server , app } from "./socket/socket.js"
+import  path from "path"
+import exp from "constants";
 dotenv.config();
 connectDB();
 // const app=express();
 
 const PORT=process.env.PORT ||5000
+const  __dirname= path.resolve();
 // to parse  json data in req.body
+
 app.use(express.json({limit:"50mb"})); 
 // to parse form data  in req.body
 app.use(express.urlencoded({extended:true}));
@@ -28,6 +32,16 @@ cloudinary.config({
 app.use("/api/users",userRoutes)
 app.use("/api/posts" ,postRoutes)
 app.use("/api/messages" ,messageRoutes)
+
+// http://localhost:5000 => bringing bacekend and frontent under same url for deploying
+if(process.env.NODE_ENV==="production")
+{ 
+ app.use(express.static(path.join(__dirname ,"/Frontend/dist")))
+//  to render react app
+ app.get("*" , (req ,res)=>{
+    res.sendFile(path.resolve(__dirname, "Frontend" ,"dist" ,"index.html"))
+ })
+}
 // app.listen(PORT , ()=>
 
 // console.log("app listening on port.." ,PORT)
@@ -38,5 +52,4 @@ app.use("/api/messages" ,messageRoutes)
 server.listen(PORT , ()=>
 
 console.log("app listening on port.." ,PORT)
- 
 )
